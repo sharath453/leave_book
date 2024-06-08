c0d108dbe6ea23bce17ce96633d387ecfe56c76e
sharath <alsharath66@gmail.com>
2024-06-08 22:40:01 +0530

    leave appliaction

--- lib/homepage/api_service.dart
+++ lib/homepage/api_service.dart
@@ -1,88 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+// lib/homepage/api_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class LeaveApplication extends StatefulWidget {
-  final String username;
+class ApiService {
+  static const String baseUrl = 'http://localhost/leave_application/';
 
-  LeaveApplication({required this.username});
-
-  @override
-  _LeaveApplicationState createState() => _LeaveApplicationState();
-}
-
-class _LeaveApplicationState extends State<LeaveApplication> {
-  final TextEditingController _leaveFromDateController =
-      TextEditingController();
-  final TextEditingController _leaveToDateController = TextEditingController();
-  final TextEditingController _reasonController = TextEditingController();
-
-  String _fromAddress = '';
-  String _toAddress = 'Manjunath Kotari';
-  String _todayDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Apply for Leave'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('From: $_fromAddress'),
-            Text('To: $_toAddress'),
-            Text('Date: $_todayDate'),
-            TextField(
-              controller: _leaveFromDateController,
-              readOnly: true,
-              onTap: () => _selectDate(context, _leaveFromDateController),
-              decoration: InputDecoration(
-                labelText: 'Leave From Date',
-              ),
-            ),
-            TextField(
-              controller: _leaveToDateController,
-              readOnly: true,
-              onTap: () => _selectDate(context, _leaveToDateController),
-              decoration: InputDecoration(
-                labelText: 'Leave To Date',
-              ),
-            ),
-            TextField(
-              controller: _reasonController,
-              decoration: InputDecoration(
-                labelText: 'Reason',
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _submitLeaveApplication,
-              child: Text('Submit'),
-            ),
-          ],
-        ),
-      ),
+  static Future<Map<String, dynamic>> login(
+      String username, String password) async {
+    final response = await http.post(
+      Uri.parse('${baseUrl}login.php'),
+      body: {
+        'username': username,
+        'password': password,
+      },
     );
-  }
 
-  Future<void> _selectDate(
-      BuildContext context, TextEditingController controller) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null) {
-      setState(() {
-        controller.text = DateFormat('yyyy-MM-dd').format(picked);
-      });
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to log in');
     }
   }
 
-  void _submitLeaveApplication() {
-    // Implement leave application submission
+  static Future<Map<String, dynamic>> getLeaveFormDetails(
+      String username) async {
+    final response = await http.post(
+      Uri.parse('${baseUrl}leave_form.php'),
+      body: {
+        'username': username,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch leave form details');
+    }
   }
 }
